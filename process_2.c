a6d990f504ff00f4887d765bd39f78a2a33d6979
Leticia <leticianascimento.cco@gmail.com>
2017-03-27 22:52:01 -0300

    add task

--- process_2.c
+++ process_2.c
@@ -6,7 +6,7 @@
 
 int main (int argc, char **argv) {
 
-    for (int i = 0; i < 3; i++) {  
+    for (int i = 0; i < 4; i++) {  
         pid_t pid = fork(); 
         if (pid > 0) {
            printf("Processo %d criou %d\n" ,getpid(), pid);
