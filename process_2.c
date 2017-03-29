b6354813f3a56fd8247c9de46ec0e54e0ba5fd0e
Leticia <leticianascimento.cco@gmail.com>
2017-03-28 21:25:03 -0300

    fixed

--- process_2.c
+++ process_2.c
@@ -10,19 +10,18 @@ int main (int argc, char **argv) {
     
     for (int i = 0; i < 4; i++) {  
         if (pid > 0) {
-            pid = fork(); 
+            pid = fork();
             if (pid > 0) {
-               printf("Processo %d criou %d\n" ,getpid(), pid);
-            } else if (pid ==0) {
+               printf("Processo %d criou %d\n", getpid(), pid);
+            } else if (pid == 0) {
                 printf("Processo filho. PID = %d\n", getpid());
                 break;
             } else {
-                printf("Processo não pode ser criado.");
+                printf("Processo não pode ser criado.\n");
                 break;
             } 
         }    
     }
-    
     //while(1) {}
     while (wait(NULL) != -1);
 }
---
+++ process_3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main (int argc, char **argv) {
+
+    pid_t pid, pid2;
+    pid = getpid();
+    int i, j;
+
+    for (i = 0; i < 2; i++) {
+        if (pid > 0) {
+            pid = fork();  // first level
+            if (pid == 0) {
+                for (j = 0; j < 2; j++) {
+                    pid2 = fork(); //second level
+                    if (pid2 == 0) {
+                        break;
+                    } else if (pid2 < 0) {
+                        printf("Processo não pode ser criado.\n");
+                        break;
+                     }    
+                } printf("Processo %d filho de %d\n", getpid(), getppid());
+                  break;     
+            } else if (pid < 0) {
+                printf("Processo não pode ser criado.\n");
+                break;
+              }
+        }
+    }
+    while (1){}
+    while (wait(NULL) != -1);
+}
