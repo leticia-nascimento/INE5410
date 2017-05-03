9c81cb331730a47b45570448b6f32e7fd97394ba
Leticia <leticianascimento.cco@gmail.com>
2017-05-03 15:12:54 -0300

    fix and finish

--- process.c
+++ process.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/* Escreva um programa em C que cria um processo utilizando
+* a chamada de sistema fork(). Ambos os processos pai e filho
+* deverão imprimir na tela a frase "Novo processo criado!". Você
+* deverá utilizar apenas um printf().
+*/
 
 int main (int argc, char **argv) {
     pird_t pid = fork();
 
     if (pid >= 0) {
       printf("Novo processo criado!\n");
-     }
-    
+    }
+
     return 0;
 }
