83b661a9d14ce8493a339697ebb4110e8d8856f8
Leticia <leticianascimento.cco@gmail.com>
2017-06-22 19:06:06 -0300

    add task

--- master_slave.c
+++ master_slave.c
@@ -20,7 +20,7 @@ int main (int argc, char *argv[]) {
 
 	int ranks[size];
 
-	if	(rank == 0) {
+	if (rank == 0) {
 		for (int i = 1; i < size; i++) {
 			MPI_Send(NULL, 0, MPI_INT, i, 0, MPI_COMM_WORLD);
 			MPI_Recv(&ranks, size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
