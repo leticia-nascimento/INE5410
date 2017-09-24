82bdd654fd3e2d57c8f6cad985052920e6a7cff3
Leticia <leticianascimento.cco@gmail.com>
2017-09-24 17:18:58 -0300

    fix

--- ResolvedorSudoku/sudokun.c
+++ ResolvedorSudoku/sudokun.c
@@ -2,102 +2,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 
 #define MAX_n 8
 
-void print(int size, int grid[][size*size]) {
-    static int solutions = 0;
-    int line, column;
+int **grid;
+int rows, columns;
 
-    printf("Solução: %d\n", ++solutions);
-        for (line = 0; line < size * size; line++) {
-            for (column = 0; column  < size * size; column++) {
-                printf(" %d", grid[line][column]);
-                if (column % size == 0) 
-                    printf("  ");
-            }
-            printf("\n");
-            if (line % size == 0)
-                printf("\n");
-        }
+int allocate_grid() {
+    grid = (int**)malloc(sizeof(int*)*rows);
+    int i;
+
+    for (i = 0; i < rows; i++)
+        grid[i] = (int*)malloc(sizeof(int)*columns);
+    
+    return grid;
 }
 
-int isValid(int line, int column, int number, int size, int grid[][size * size]) {
-        int i, j, localLine, localColumn;
+void free_grid() {
+    int i;
 
-        if (grid[line][column] == number)
-            return 1;
-        if (grid[line][column] != 0)
-            return 0;
-        for (i = 0; i < size * size; i++) {
-            if (grid[line][i] == number)
-                return 0;
-        }
-        for (j = 0; j < size * size; j++) {
-            if(grid[j][column] == number)
-                return 0;
-        }
+    for (i = 0; i < rows; i++)
+        free(grid[i]);
 
-        localLine = line/size;
-        localColumn = column/size;
+    free(grid);
+}
 
-        for (i = localLine * size; i < (localLine + 1) * size; i++) {
-            for (j = localColumn * size; j < (localColumn + 1) * size; j++) {
-                if(grid[i][j] == number)
-                    return 0;
-            }
+void convert(int size, int buf[]) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            grid[i][j] = buf[i * (size*size) + j];
         }
+    }
+    print();
+}
+
+// checks if a number belongs to a cell
+int check(int row, int column, int number) {
+    int square = (int)(sqrt(rows));
+
+    if (grid[row][column] == number) return 1;
+    if (grid[row][column] != 0) return 0;
+
+    for (int j = 0; j < columns; j++)
+        if (grid[row][j] == number) return 0;
+
+    for (int i = 0; i < rows; i++)
+        if (grid[i][column] == number) return 0;
+
+    int sector_row = row / square;
+    int sector_column = column / square;
+
+    for (int i = sector_row * square; i < (sector_row + 1) * square; ++i)
+        for (int j = sector_column * square; j < (sector_column + 1) * square; ++j)
+            if (grid[i][j] == number) return 0;
 
     return 1;
 }
 
-void solveSudoku(int line, int column, int size, int grid[][size*size]) {
-    int number, temp;
+void solve(int row, int column) {
+    int number, attempt;
 
-    if (line == 9)
-        print(size, grid);
-    else
-        for (number = 1; number <= size * size; number++) {
-            if (isValid(line, column, number, size, grid)) {
-                temp = grid[line][column];
-                grid[line][column] = number;
-
-                if (column == (size * size) - 1)
-                    solveSudoku(line + 1, 0, size, grid);
+    if (row == rows) {
+        print();
+    } else {
+        for (number = 1; number <= 9; number++) {
+            if (check(row, column, number)) {
+                attempt = grid[row][column];
+                grid[row][column] = number;
+                if (column == columns - 1)
+                    solve(row + 1, 0);
                 else
-                    solveSudoku(line, column + 1, size, grid);
-                grid[line][column] = temp;
+                    solve(row, column + 1);
+                grid[row][column] = attempt;
             }
         }
+    }
 }
 
 
-int main (int argc, char **argv) {
-  int size;
-  assert(scanf("%d", &size) == 1);
-  assert (size <= MAX_n);
+int main(int argc, char argv) {
+    int size;
+    assert(scanf("%d", &size) == 1);
+    assert (size <= MAX_n);
 
-  int buf_size = size * size * size * size;
-  int buf[buf_size];
+    int buf_size = size * size * size * size;
+    int buf[buf_size];
 
-  for (int i = 0; i < buf_size; i++) {
-    if (scanf("%d", &buf[i]) != 1) {
-      printf("error reading file (%d)\n", i);
-      exit(1);
-    }
-  }
-
-  int line = size * size;
-  int column = size * size;
-  int grid[line][column];
-
-    for (int i = 0; i < size * size; i++) {
-        for (int j = 0; j < size * size; j++) {
-            grid[line][column] = buf[i * (size * size) + j];
+    for (int i = 0; i < buf_size; i++) {
+        if (scanf("%d", &buf[i]) != 1) {
+        printf("error reading file (%d)\n", i);
+        exit(1);
         }
     }
 
-  solveSudoku(0, 0, size, grid);
-
-  return 0;
+    rows = size*size;
+    columns = size*size;
+    allocate_grid();
+    convert(size, buf);
+    free_grid();
+    // printBuf(buf, size);
 }
+
+void print() {
+    int row, column;
+    static int solutions = 0;
+
+    printf("Solutions: %d\n", ++solutions);
+    for (row = 0; row < rows; row++) {
+        for (column = 0; column < columns; column++) {
+            printf (" %d",grid[row][column]);
+            // xx
+            if (column % columns == (columns - 1)) printf ("  ");
+        }
+        //xx
+        printf ("\n");
+        if (row % rows == (rows - 1)) printf ("\n");
+    }
+}
+
+void printBuf(int buf[], int size) {
+    int size_squared = size * size;
+    printf("This is the buf\n");
+    for (int i = 0; i < size_squared; i++) {
+        for (int j = 0; j < size_squared; j++)
+        printf(" %d ", buf[i * size_squared + j]);
+
+        printf("\n");
+    }
+}
+
